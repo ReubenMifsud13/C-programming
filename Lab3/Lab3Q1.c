8ba7774dfec2497c9465426f3c99db01e18b788d
ReubenMifsud13 <reuben.mifsud.24@um.edu.mt>
2024-11-13 18:14:48 +0100

    update Lab3Q1.c

--- Lab3/Lab3Q1.c
+++ Lab3/Lab3Q1.c
@@ -6,7 +6,7 @@ int main(void)
     long sum = 0L; /* initialize sum to zero */
     int status;
 
-    printf("Please enter next integer (q to quit): ");
+    printf("Please enter integer (q to quit): ");
 
     while(scanf("%ld", &num) == 1){
         sum = sum + num;
